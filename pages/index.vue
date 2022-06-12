b15947ea067e10e4acbe5dca8685f5a3b8ac8c86
Victor Abbah Nkoms <veekthoven@gmail.com>
2022-06-12 16:48:38 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        MailerLite
+      </h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
